988d8e021e76387ce6047efab95b8185cd1f9e29
onval <giorgio.valbonesi@gmail.com>
2016-11-19 14:54:56 +0100

    Add detail activity and fragment

---
+++ app/src/main/java/com/onval/popular_movies_1/DetailActivity.java
@@ -0,0 +1,34 @@
+package com.onval.popular_movies_1;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_detail, new DetailFragment())
+                    .commit();
+        }
+    }
+
+    public static class DetailFragment extends Fragment {
+        public DetailFragment() { }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_detail, container, false);
+        }
+    }
+}
