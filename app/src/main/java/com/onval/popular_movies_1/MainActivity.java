8c501329fd56b1b897caba9abf6bf62d4f39f403
onval <giorgio.valbonesi@gmail.com>
2016-11-19 15:12:32 +0100

    Refactor placeholder_fragment into fragment_main

--- app/src/main/java/com/onval/popular_movies_1/MainActivity.java
+++ app/src/main/java/com/onval/popular_movies_1/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.placeholder_fragment, container, false);
+            return inflater.inflate(R.layout.fragment_main, container, false);
         }
     }
 }
