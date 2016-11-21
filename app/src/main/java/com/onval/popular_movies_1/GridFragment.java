94ec655db5aae42a8c1529d06bbb74b9d561594f
onval <giorgio.valbonesi@gmail.com>
2016-11-21 23:58:42 +0100

    Populate GridView with dummy images

--- app/src/main/java/com/onval/popular_movies_1/GridFragment.java
+++ app/src/main/java/com/onval/popular_movies_1/GridFragment.java
@@ -6,22 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class GridFragment extends Fragment {
-    public ArrayAdapter<String> adapter;
-    public String[] data = {
-            "derp", "lol", "derpino",
-            "lollino", "lollone", "ahah",
-            "well", "wellone"
-    };
+    final static String PICASSO_URL = "http://i.imgur.com/DvpvklR.png";
+    final static String CAT_URL = "https://www.petdrugsonline.co.uk/images/page-headers/cats-master-header";
 
-    public List<String> stringList = new ArrayList<>(Arrays.asList(data));
+    public ThumbnailAdapter adapter;
 
     public GridFragment() {
         // Required empty public constructor
@@ -31,7 +25,14 @@ public class GridFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        adapter = new ArrayAdapter<>(getContext(), R.layout.textview, stringList);
+
+        //four picasso images, just to populate it with something
+        String[] urls = {
+            PICASSO_URL, CAT_URL, PICASSO_URL, CAT_URL
+        };
+
+        adapter = new ThumbnailAdapter(getContext(), new ArrayList<>(Arrays.asList(urls)));
+
         GridView gridView = (GridView) rootView.findViewById(R.id.grid_view);
         gridView.setAdapter(adapter);
 
--- app/src/main/java/com/onval/popular_movies_1/MainActivity.java
+++ app/src/main/java/com/onval/popular_movies_1/MainActivity.java
@@ -3,8 +3,7 @@ package com.onval.popular_movies_1;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
