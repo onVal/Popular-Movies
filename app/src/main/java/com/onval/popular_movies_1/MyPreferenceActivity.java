dff4026c45ef3fdf9c01cf7f479b4a30b46abd44
onval <giorgio.valbonesi@gmail.com>
2016-11-26 15:09:31 +0100

    Bind summary to chose value in preferences

--- app/src/main/java/com/onval/popular_movies_1/MyPreferenceActivity.java
+++ app/src/main/java/com/onval/popular_movies_1/MyPreferenceActivity.java
@@ -1,60 +1,55 @@
 package com.onval.popular_movies_1;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 /**
  * Created by gval on 26/11/16.
  */
 
-public class MyPreferenceActivity extends PreferenceActivity {
+public class MyPreferenceActivity extends PreferenceActivity
+    implements Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new MyPreferenceFragment()).commit();
-//        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_sort_key)));
+        addPreferencesFromResource(R.xml.my_preferences);
+        Preference pref = findPreference(getString(R.string.pref_sort_key));
+        bindPreferenceSummaryToValue(pref);
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.my_preferences);
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
         }
+        return true;
     }
-
-//    private void bindPreferenceSummaryToValue(Preference preference) {
-//        // Set the listener to watch for value changes.
-//        preference.setOnPreferenceChangeListener(this);
-//
-//        // Trigger the listener immediately with the preference's
-//        // current value.
-//        onPreferenceChange(preference,
-//                PreferenceManager
-//                        .getDefaultSharedPreferences(preference.getContext())
-//                        .getString(preference.getKey(), ""));
-//    }
-
-//    @Override
-//    public boolean onPreferenceChange(Preference preference, Object value) {
-//        String stringValue = value.toString();
-//
-//        if (preference instanceof ListPreference) {
-//            // For list preferences, look up the correct display value in
-//            // the preference's 'entries' list (since they have separate labels/values).
-//            ListPreference listPreference = (ListPreference) preference;
-//            int prefIndex = listPreference.findIndexOfValue(stringValue);
-//            if (prefIndex >= 0) {
-//                preference.setSummary(listPreference.getEntries()[prefIndex]);
-//            }
-//        } else {
-//            // For other preferences, set the summary to the value's simple string representation.
-//            preference.setSummary(stringValue);
-//        }
-//        return true;
-//    }
 }
