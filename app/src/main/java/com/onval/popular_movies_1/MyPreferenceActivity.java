5c866197083eeacd8a071a7284b93b30f5bc96de
onval <giorgio.valbonesi@gmail.com>
2016-11-26 12:30:46 +0100

    Add most preferences related files

---
+++ app/src/main/java/com/onval/popular_movies_1/MyPreferenceActivity.java
@@ -0,0 +1,17 @@
+package com.onval.popular_movies_1;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by gval on 26/11/16.
+ */
+
+public class MyPreferenceActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        if (savedInstanceState != null) {
+        }
+    }
+}
