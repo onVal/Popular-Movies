84a0e1f0cee1a8d26ee481581b1e6753b7f369ff
onval <giorgio.valbonesi@gmail.com>
2016-11-26 05:59:11 +0100

    Start adding menu

--- app/src/main/java/com/onval/popular_movies_1/MainActivity.java
+++ app/src/main/java/com/onval/popular_movies_1/MainActivity.java
@@ -2,6 +2,8 @@ package com.onval.popular_movies_1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity  {
     @Override
@@ -14,5 +16,17 @@ public class MainActivity extends AppCompatActivity  {
                     .add(R.id.activity_main, new GridFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
