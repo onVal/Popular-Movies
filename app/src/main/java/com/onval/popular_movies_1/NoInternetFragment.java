cbd4bd4c8e70f35343254ce48cbf8cc2840acc8a
onval <giorgio.valbonesi@gmail.com>
2016-11-28 15:01:40 +0100

    Add refresh button to the NoInternetFragment

--- app/src/main/java/com/onval/popular_movies_1/NoInternetFragment.java
+++ app/src/main/java/com/onval/popular_movies_1/NoInternetFragment.java
@@ -1,11 +1,13 @@
 package com.onval.popular_movies_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by gval on 28/11/16.
@@ -19,6 +21,14 @@ public class NoInternetFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_no_internet, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_no_internet, container, false);
+        Button btn = (Button) rootView.findViewById(R.id.refresh_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), MainActivity.class));
+            }
+        });
+        return rootView;
     }
 }
