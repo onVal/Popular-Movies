caa173464b66f3050e5caa33027c69a3e69484aa
onval <giorgio.valbonesi@gmail.com>
2017-08-30 23:42:51 +0200

    Fix movie duplicates on back-button

--- app/src/main/java/com/onval/popular_movies/MovieFragment.java
+++ app/src/main/java/com/onval/popular_movies/MovieFragment.java
@@ -83,7 +83,9 @@ public class MovieFragment extends Fragment implements
         loadMoreMoviesFab.setOnClickListener(this);
 
         presenter = new MoviePresenter(this);
-        presenter.fetchMoviesAsync();
+
+        if (mMovieDetailsArray.size() == 0)
+            presenter.fetchMoviesAsync();
 
         if (moviesAdapter == null)
             moviesAdapter = new RecyclerAdapter(mContext, mMovieDetailsArray, this); //todo:maybe refactor this method
@@ -101,31 +103,6 @@ public class MovieFragment extends Fragment implements
         intent.putExtra(MovieDetail.MOVIE_DETAILS_ID, mMovieDetailsArray.get(position));
         startActivity(intent);
     }
-
-//    @Override
-//    public void onItemClick(FavoritesAdapter.FavPosterHolder holder) {
-//        int position = holder.getAdapterPosition();
-//        Intent intent = new Intent(mContext, DetailActivity.class);
-//
-//        MovieDetail movieDetail = movieDetailFromCursor(favCursor, position);
-//
-//        intent.putExtra(MovieDetail.MOVIE_DETAILS_ID, movieDetail);
-//        startActivity(intent);
-//    }
-
-//    MovieDetail movieDetailFromCursor(Cursor cursor, int position) {
-//        cursor.moveToPosition(position);
-//
-//        return new MovieDetail(
-//                cursor.getInt(cursor.getColumnIndex(MovieContract.Favorites._ID)),
-//                cursor.getString(cursor.getColumnIndex(MovieContract.Favorites.TITLE_COLUMN)),
-//                cursor.getString(cursor.getColumnIndex(MovieContract.Favorites.POSTERPATH_COLUMN)),
-//                cursor.getString(cursor.getColumnIndex(MovieContract.Favorites.OVERVIEW_COLUMN)),
-//                cursor.getDouble(cursor.getColumnIndex(MovieContract.Favorites.VOTE_AVG_COLUMN)),
-//                cursor.getDouble(cursor.getColumnIndex(MovieContract.Favorites.POPULARITY_COLUMN)),
-//                cursor.getString(cursor.getColumnIndex(MovieContract.Favorites.RELEASE_DATE_COLUMN))
-//        );
-//    }
 
     //onClick for FAB
     @Override
